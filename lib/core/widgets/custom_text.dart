7c9e127e853b3697242eafb4810b9512a7777b62
Ehabhanna1 <ehabhanna1599@gmail.com>
2025-05-09 17:09:34 +0300

    add home view app bar

---
+++ lib/core/widgets/custom_text.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustomText extends StatelessWidget {
+  const CustomText({super.key, required this.text,  this.color = Colors.white,  this.maxLines,  this.fontWeight,  this.fontSize});
+  final String text;
+  final Color color;
+  final int? maxLines;
+  final FontWeight? fontWeight;
+  final double? fontSize;
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text,
+    maxLines: maxLines,
+    style: TextStyle(
+      color: color,
+      fontSize: fontSize,
+      fontWeight: fontWeight,
+     
+    ),);
+  }
+}
--- lib/features/home_screen/home_screen.dart
+++ lib/features/home_screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,11 +9,63 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController searchController = TextEditingController();
+  FocusNode focusNode = FocusNode();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    focusNode.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home Screen"),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            children: [
+              Image.asset('assets/images/youtube.png',width: 36, ),
+              const SizedBox(width: 8,),
+              const Text('YouTube', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 21),),
+              SizedBox(width: 10,),
+              Expanded(
+                child: CupertinoTextField(
+                  controller: searchController,
+                  placeholder: "Search...",
+                  placeholderStyle: TextStyle(color: Colors.black54),
+                  prefix: Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Icon(CupertinoIcons.search, color: Colors.black54,size: 20,),
+                  ),
+                  suffix:  Padding(
+                    padding: const EdgeInsets.only(right: 8),
+                    child: Icon(Icons.cancel_outlined, color: Colors.redAccent,size: 20,),
+                  ),
+                  style: TextStyle(color: Colors.black),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  onTap: () {},
+                  onSubmitted: (value) {},
+                ),
+              ),
+              
+            ],
+          ),
+        ),
       ),
     );
   }
